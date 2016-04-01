facedf7bd261920fce27266439685595242a412c
Michael Bruce <michael.bruce@singletracksystems.com>
2016-04-01 16:35:40 +0100

    config-file slurped into an array by line

--- src/clojureforce/core.clj
+++ src/clojureforce/core.clj
@@ -1,4 +1,10 @@
-(ns clojureforce.core)
+(ns clojureforce.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def config-file (io/file (io/resource "oauth.yml")))
+
+(str/split (slurp config-file) #"\n")
 
 (defn foo
   "I don't do a whole lot."
